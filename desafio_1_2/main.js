ea89354d506ea4da59fb2d6666c9c26d81d0edbb
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-03-10 11:50:38 -0300

    Desafio 1.2: Esqueleto do menu principal

--- desafio_1_2/main.js
+++ desafio_1_2/main.js
@@ -2,16 +2,30 @@
 import readlineSync from 'readline-sync';
 import { PacienteService } from './Services/PacienteService.js';
 
-function main() {
-  let controlador = true;
-  while (controlador == true) {
+function menuPaciente(){
+  let controladorPaciente = readlineSync.questionInt('\nMenu do Cadastro de Paciente \n 1-Cadastrar novo paciente \n 2-Excluir paciente \n 3-Listar pacientes (ordenado por CPF) \n 4-Listar pacientes (ordenado por nome) 5-Voltar p/ menu principal');
+  if(controladorPaciente == 1){
     const servicePaciente = new PacienteService();
-    console.log('Menu Principal');
     //let cpf = readlineSync.question('Digite o CPF do Paciente: ');
     const paciente = servicePaciente.criarPaciente();
     console.log(servicePaciente.cadastroDeCpf(paciente, '06010634300'));
     console.log(paciente);
-    controlador = false;
+  }
+}
+
+function main() {
+  let controlador = true;
+  while (controlador == true) {
+    let menuPrincipal = readlineSync.questionInt('Menu Principal \n 1-Cadastro de pacientes \n 2-Agenda \n 3-Fim');
+    if(menuPrincipal == 1){
+      menuPaciente();
+    }
+    if(menuPrincipal == 2){
+      controlador = false;
+    }
+    if(menuPrincipal == 3){
+      controlador = false;
+    }
   }
 }
 
